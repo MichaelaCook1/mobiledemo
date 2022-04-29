3c83a9702ce1eb24c48c0d0e2f9dca8f0241d69b
MichaelaCook1 <mcook1@qa.com>
2022-04-29 11:59:49 +0100

    adding src from qr branch

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+
+import React from 'react';
+import Html5QrcodePlugin from './Html5QrcodePlugin.jsx'
+import ResultContainerPlugin from './ResultContainerPlugin.jsx'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      decodedResults: []
+    }
+
+    // This binding is necessary to make `this` work in the callback.
+    this.onNewScanResult = this.onNewScanResult.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <section className = "App-section">
+          <div className = "App-section-title"> Scan Code </div>
+          <Html5QrcodePlugin 
+            fps={10}
+            qrbox={250}
+            disableFlip={false}
+            qrCodeSuccessCallback={this.onNewScanResult}/>
+          <ResultContainerPlugin results={this.state.decodedResults} />
+          </section>
+      </div>
+    );
+  }
+
+  onNewScanResult(decodedText, decodedResult) {
+    console.log(
+      "App [result]", decodedResult);
+
+    // let decodedResults = this.state.decodedResults;
+    // decodedResults.push(decodedResult);
+    this.setState((state, props) => {
+      state.decodedResults.push(decodedResult);
+      return state;
+    });
+  }
+}
+
+export default App;
