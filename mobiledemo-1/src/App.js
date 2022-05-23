938975c19c908708300e7ce38f21e9c4378515f2
samkazshel <samsheldon779@gmail.com>
2022-05-23 16:42:05 +0100

    added files for nfc implementation currently not working due to problems with useState

--- mobiledemo-1/src/App.js
+++ mobiledemo-1/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 
 import React from 'react';
+import Scan from "./Scan";
+import { useState } from 'react';
+import { ActionsContext } from './context';
 import Html5QrcodePlugin from './Html5QrcodePlugin.jsx'
 import ResultContainerPlugin from './ResultContainerPlugin.jsx'
 
@@ -18,6 +21,15 @@ const hSubmit = function(event){
 
   console.log( { value });
   
+  }
+
+  const [actions, setActions] = useState(null);
+  const {scan} = actions || {};
+
+  const actionValue = {actions, setActions};
+
+  const onHandleAction = (actions) =>{
+    setActions({...actions})
   }
 
 class App extends React.Component {
@@ -64,6 +76,14 @@ class App extends React.Component {
         <p>
         <label>Seizure Details:  </label>
         <textarea name="seizure-content" rows="2" cols="20"></textarea>
+
+        <h1>NFC Scan</h1>
+        <div classname="App-container">
+          <button onClick={()=>onHandleAction({scan: 'scanning', write: null})} className="btn">Scan</button>
+        </div>
+        <ActionsContext.Provider value={actionValue}>
+          {scan && <Scan/>}
+        </ActionsContext.Provider>
         
         <p><img id="output" width="200"/></p>
         </p>
